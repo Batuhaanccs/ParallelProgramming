fdb9fb35aeb690a1441f9085918384128479b57c
rft0 <rafos111@hotmail.com>
2026-03-28 20:04:20 +0300

    more

--- Week04/decorators_rafet_taskin.py
+++ Week04/decorators_rafet_taskin.py
@@ -1,27 +1,22 @@
-import time
 import tracemalloc
+import time
 import functools
 
 def performance(func):
+    performance.counter = 0
+    performance.total_time = 0.0
+    performance.total_mem = 0.0
+
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         tracemalloc.start()
         start_time = time.time()
-        
-        try:
-            result = func(*args, **kwargs)
-        finally:
-            current, peak = tracemalloc.get_traced_memory()
-            tracemalloc.stop()
-            end_time = time.time()
-            
-            wrapper.counter += 1
-            wrapper.total_time += (end_time - start_time)
-            wrapper.total_mem += peak
-            
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        current, peak = tracemalloc.get_traced_memory()
+        tracemalloc.stop()
+        performance.counter += 1
+        performance.total_time += (end_time - start_time)
+        performance.total_mem += peak
         return result
-        
-    wrapper.counter = 0
-    wrapper.total_time = 0
-    wrapper.total_mem = 0
     return wrapper
